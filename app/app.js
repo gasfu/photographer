abb704f9c5ba1c24389a6447a33fb6bb6626a1c6
Gabriel Furlan <gabrielfurlan05@gmail.com>
2017-04-14 21:47:53 -0300

    addded funciton send email

--- app/app.js
+++ app/app.js
@@ -2,21 +2,28 @@ const axios = require('axios');
 const form = document.querySelector("#contactForm");
 
 function submit (){
-	const data = {
 
+	const form = document.forms.contact;
+	console.log(form.body.value);
+
+	const data = {
+		'from': form.email.value,
+		'name': form.name.value,
+		'phone': form.phone.value,
+		'body': form.body.value 
 	};
 
-	axios.get('mailer/mail.php', data).then(function (response){
-		console.log(response);
+	const str = JSON.stringify(data);
+
+	axios.get('mailer/mail.php', { params: data}).then(function (response){
+		console.log(response.data);
 	});
 
-	console.log('teste')
 	return false;
 	
 }
 
 form.addEventListener('submit', function(e) {
 	e.preventDefault();
-	console.log('teste');
 	return submit();
 });
--- public/mailer/mail.php
+++ public/mailer/mail.php
@@ -1,5 +1,46 @@
 <?php
+	header('Content-type: text/html; charset=UTF-8');
+	
+	require "phpmailer/PHPMailerAutoload.php";
+	email('teste@thiagofotografia.com.br');
 
-	echo "teste";
+	function email($destinatario){
 
+		$mail = new PHPMailer();
+		$mail->IsSMTP();
+		$mail->CharSet = "UTF-8";
+
+		$mail->SMTPAuth = true;
+		$mail->SMTPSecure = false;
+		$mail->isHTML(true);
+
+		$mail->Host = 'br664.hostgator.com.br';
+		$mail->Port = 26;
+
+		$mail->Username = 'send@thiagofotografia.com.br';
+		$mail->Password = 'send@thiago@123';
+
+		$mail->From = utf8_decode($_GET['from']);
+		$mail->FromName = utf8_decode($_GET['name']);
+		$mail->addReplyTo(utf8_decode($_GET['from']), utf8_decode($_GET['name']));
+		$mail->Subject = "[[SITE]] | ". utf8_decode($_GET['name']);
+				
+		$mail->Body = "<strong>Contato pelo site de: </strong>". utf8_decode($_GET['name']);
+		$mail->Body .= "<br>";
+		$mail->Body .= "<strong>Telefone: </strong>". utf8_decode($_GET['phone']);				
+		$mail->Body .= "<br>";
+		$mail->Body .= "<br>";
+		$mail->Body .= "<strong>Menssagem: </strong> ";				
+		$mail->Body .= utf8_decode($_GET['body']);
+		$mail->Body .= "<br>";
+		$mail->Body .= "<br>";
+		$mail->AddAddress($destinatario);
+		
+		if($mail->send()) 
+			$response = array("send" => true, "message" => "Email enviado com sucesso!", "subMessage" => "Logo entraremos em contato.");
+		else
+			$response = array("send" => false, "message" => "Erro!", "subMessage" => $mail->ErrorInfo);
+
+		echo json_encode($response);
+	}
 ?>
